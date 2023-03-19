a51a0d2ee5ff0f57b944ee37fc65e90365bb08a5
Chintan Joshi <chintan.joshi@hubparking.com>
2023-03-19 03:17:10 -0400

    Added logging

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -1,14 +1,24 @@
 ﻿
 using TestLib;
+using log4net;
+using JetBrains.Annotations;
 
 namespace MainApp;
 public class Program{
+    
+    [NotNull]
+    private static readonly ILog log = LogManager.GetLogger(typeof(MyClass));
+
     static void Main(string[] args) {
+
+        log4net.Config.XmlConfigurator.Configure(new System.IO.FileInfo("log4net.config"));
 
         var myClass = new MyClass();
         myClass.MyMethod(1, "Chintan", DateTime.Now);    
-
         Console.WriteLine($"{myClass.MyStringMember}, you passed {myClass.MyIntMember} at {myClass.MyDateTimeMember}");
+        log.Info("This is info message");
+        log.Warn("This is warning message");
+        log.Error("This is error message");
     }
 }
     
--- TestLib/MyClass.cs
+++ TestLib/MyClass.cs
@@ -1,17 +1,32 @@
-﻿namespace TestLib;
+﻿using JetBrains.Annotations;
+using log4net;
+
+namespace TestLib;
 
 public class MyClass
 { 
+    [NotNull]
+    private static readonly ILog log = LogManager.GetLogger(typeof(MyClass));
+
     private int myIntMember;
+    
     private String myStringMember;
+    
+    [NotNull]
     public String MyStringMember {get=>myStringMember; private set => myStringMember = value;}
+    
+    [NotNull]
     public int MyIntMember { get => myIntMember; private set => myIntMember = value; }
+    
+    [NotNull]
     public DateTime MyDateTimeMember {get; private set;}
-    public void MyMethod(int intMember, String stringMember, DateTime dateTimeMember){
 
-     this.MyIntMember = intMember;
-     this.MyStringMember = stringMember;
-     this.MyDateTimeMember = dateTimeMember;
+    public void MyMethod([NotNull]int intMember, [NotNull]String stringMember, [NotNull]DateTime dateTimeMember){
 
+        this.MyIntMember = intMember;
+        this.MyStringMember = stringMember;
+        this.MyDateTimeMember = dateTimeMember;
+
+        log.Debug("Initialized my class...");
     }
 }
