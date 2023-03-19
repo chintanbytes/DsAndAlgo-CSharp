ec9c2b1e6fabbab931ae835e66fdcfdc777f731d
Chintan Joshi <chintan.joshi@hubparking.com>
2023-03-19 19:16:44 -0400

    Adding a test project

--- MainApp/Program.cs
+++ MainApp/Program.cs
@@ -4,24 +4,25 @@ using log4net;
 using JetBrains.Annotations;
 
 namespace MainApp;
-public class Program {
-    
+public class Program
+{
     //Get logger
     [NotNull]
     private static readonly ILog log = LogManager.GetLogger(typeof(Program));
 
     //Main entry point of the application
-    static void Main(string[] args) {
+    static void Main(string[] args)
+    {
 
         //Load log4net configuration
         log4net.Config.XmlConfigurator.Configure(new System.IO.FileInfo("log4net.config"));
 
-        var myClass = new MyClass(1, "Chintan", DateTime.Now);
-        myClass.MyTestClassMethod();    
+        var myClassInstance = new MyClass(1, "Chintan", DateTime.Now);
+        myClassInstance.MyTestClassMethod();
 
         //Output to console
-        Console.WriteLine($"{myClass.MyStringMember}, you passed {myClass.MyIntMember} at {myClass.MyDateTimeMember}");
-        
+        Console.WriteLine($"{myClassInstance.MyStringMember}, you passed {myClassInstance.MyIntMember} at {myClassInstance.MyDateTimeMember}");
+
         //Testing various log messages with different log levels
         log.Info("This is info message");
         log.Warn("This is warning message");
--- MyLib/MyClass.cs
+++ MyLib/MyClass.cs
@@ -6,25 +6,25 @@ namespace MyLib;
 
 //First test class to test with my applications
 public class MyClass
-{ 
+{
     [NotNull]
     private static readonly ILog log = LogManager.GetLogger(typeof(MyClass));
 
     private int myIntMember;
-    
+
     private String myStringMember;
-    
+
     [NotNull]
-    public String MyStringMember {get=>myStringMember; private set => myStringMember = value;}
-    
+    public String MyStringMember { get => myStringMember; private set => myStringMember = value; }
+
     [NotNull]
     public int MyIntMember { get => myIntMember; private set => myIntMember = value; }
-    
+
     [NotNull]
-    public DateTime MyDateTimeMember {get; private set;}
+    public DateTime MyDateTimeMember { get; private set; }
 
     //Public constructor
-    public MyClass([NotNull]int intMember, [NotNull]String stringMember, [NotNull]DateTime dateTimeMember)
+    public MyClass([NotNull] int intMember, [NotNull] String stringMember, [NotNull] DateTime dateTimeMember)
     {
         this.MyIntMember = intMember;
         this.MyStringMember = stringMember;
@@ -34,7 +34,8 @@ public class MyClass
     }
 
     //Test method for the first class
-    public void MyTestClassMethod(){
+    public void MyTestClassMethod()
+    {
         log.Debug($"Inside {MethodInfo.GetCurrentMethod().Name}");
     }
 }
