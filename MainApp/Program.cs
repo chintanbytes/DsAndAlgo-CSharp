a83edc5af4f77b6f4c870499bcb7dcdb41b13fec
Chintan Joshi <chintan.joshi@hubparking.com>
2023-03-18 23:57:50 -0400

    First Commit

---
+++ MainApp/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using TestLib;
+
+namespace MainApp;
+public class Program{
+    static void Main(string[] args) {
+
+        var myClass = new MyClass();
+        myClass.MyMethod(1, "Chintan", DateTime.Now);    
+
+        Console.WriteLine($"{myClass.MyStringMember}, you passed {myClass.MyIntMember} at {myClass.MyDateTimeMember}");
+    }
+}
+    
---
+++ TestLib/MyClass.cs
@@ -0,0 +1,17 @@
+﻿namespace TestLib;
+
+public class MyClass
+{ 
+    private int myIntMember;
+    private String myStringMember;
+    public String MyStringMember {get=>myStringMember; private set => myStringMember = value;}
+    public int MyIntMember { get => myIntMember; private set => myIntMember = value; }
+    public DateTime MyDateTimeMember {get; private set;}
+    public void MyMethod(int intMember, String stringMember, DateTime dateTimeMember){
+
+     this.MyIntMember = intMember;
+     this.MyStringMember = stringMember;
+     this.MyDateTimeMember = dateTimeMember;
+
+    }
+}
